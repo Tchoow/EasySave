f822fc7faa1262c4e10f8eabaccc36783a60d81d
Theo <45401519+Tchoow@users.noreply.github.com>
2023-01-24 10:46:54 +0100

    feat : project main

---
+++ EasySave.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace EasySave
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
