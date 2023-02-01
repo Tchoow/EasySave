17cb6a4e13bb5f177e1a695587c19ebd98c8d0fc
Tchoow <theo.lavie@live.fr>
2023-02-01 13:22:09 +0100

    Procject structure UML

---
+++ EasySave/EasySave/Launcher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave
+{
+    class Launcher
+    {
+
+        static void Main(string[] args)
+        {
+            View view = new View();
+            view.init();
+        }
+    }
+}
