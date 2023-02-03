dd0c76f1543f40b137f97b1771e733fc660c39ee
Tyosa720 <theophile.lioppe@viacesi.fr>
2023-02-03 15:26:47 +0100

    feat():create and update logfile

--- EasySave/EasySave/Launcher.cs
+++ EasySave/EasySave/Launcher.cs
@@ -9,6 +9,14 @@ namespace EasySave
 
         static void Main(string[] args)
         {
+            /*Log logs = new Log("name",
+                    "fileSource",
+                    "fileTarget",
+                    "destPath",
+                    4552,
+                   646
+                   );*/
+            logs.saveLogInFile();
             View view = new View();
             view.init();
         }
--- EasySave/EasySave/Log.cs
+++ EasySave/EasySave/Log.cs
@@ -1,45 +1,135 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Json.Net;
 
 namespace EasySave
 {
     class Log
     {
-        private DateTime          time;
-        private string            name;
-        private string      fileSource;
-        private string      fileTarget;
-        private string        destPath;
-        private int           fileSize;
-        private int   fileTransferTime;
+        private DateTime _time;
+        private string _name;
+        private string _fileSource;
+        private string _fileTarget;
+        private string _destPath;
+        private int _fileSize;
+        private int _fileTransferTime;
 
-        public Log(string name,
+
+        public DateTime time
+        {
+            get { return _time; }
+            set { _time = value; }
+        }
+
+        public  string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+
+        public string fileSource
+        {
+            get { return _fileSource; }
+            set { _fileSource = value; }
+        }
+
+     
+
+        public string fileTarget
+        {
+            get { return _fileTarget; }
+            set { _fileTarget = value; }
+        }
+
+
+       
+
+        public string destPath
+        {
+            get { return _destPath; }
+            set { _destPath = value; }
+        }
+       
+
+        public int fileSize
+        {
+            get { return _fileSize; }
+            set { _fileSize = value; }
+        }
+
+        public int fileTransferTime
+        {
+            get { return _fileTransferTime; }
+            set { _fileTransferTime = value; }
+        }
+
+        
+
+        public Log(
+                   string name,
                    string fileSource,
                    string fileTarget,
                    string destPath,
                    int fileSize,
-                   int fileTransferTime)
+                   int fileTransferTime
+             )
         {
-            this.time = new DateTime();
-            this.name = name;
-            this.fileSource = fileSource;
-            this.fileTarget = fileTarget;
-            this.destPath   = destPath;
-            this.fileSize   = fileSize;
-            this.fileTransferTime = fileTransferTime;
+            this._time = DateTime.Now;
+            this._name = name;
+            this._fileSource = fileSource;
+            this._fileTarget = fileTarget;
+            this._destPath   = destPath;
+            this._fileSize   = fileSize;
+            this._fileTransferTime = fileTransferTime;
         }
 
-        public bool storeLogInFile()
-        {
-            // if the file exist
-            // if not create one
-            // if yes fill eat with infos
-            // return true
-
-            return false;
-        }
         
+
+       
+
+        public void saveLogInFile()
+        {
+            DateTime currentDate = DateTime.Now;
+            string namefile = currentDate.ToString("MM-dd-yyyy") + ".json";
+            string projectPath = Path.GetFullPath(@"../../../");
+            string logsFilePath = Path.Combine(projectPath, @"datas/logs/", namefile);
+
+            if (!File.Exists(logsFilePath))
+            {
+                using (FileStream fs = File.Create(logsFilePath))
+                {
+                  
+                }
+            }
+            List<Log> jsonObj = JsonConvert.DeserializeObject<List<Log>>(File.ReadAllText(logsFilePath));
+            if (jsonObj == null) jsonObj = new List<Log>();
+            
+            jsonObj.Add(this);
+            File.WriteAllText(logsFilePath, JsonConvert.SerializeObject(jsonObj, Formatting.Indented));
+
+
+
+
+
+
+
+
+
+            //string jsonFormated = JsonConvert.SerializeObject(this, Formatting.Indented);
+            //File.AppendAllText(logsFilePath, jsonFormated);
+
+
+
+
+        }
+
 
     }
 }
