4dc8fa0c6c8bf490f19d766ed5e3f7b37acfc19e
Tyosa720 <theophile.lioppe@viacesi.fr>
2023-02-07 17:25:02 +0100

    feat() : class settings + singleton

---
+++ EasySave/EasySave/Settings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasySave
+{
+    class Settings
+    {
+        private static Settings instanceSingleton = null;
+        private Translate Language{get; set;}
+        Settings()
+        {
+
+        }
+        public static Settings InstanceSingleton
+        {
+            get
+            {
+                if (instanceSingleton == null)
+                {
+                    instanceSingleton = new Settings();
+                }
+                return instanceSingleton;
+            }
+        }
+    }
+}
