5ada8babe13c1a9f3a4858777191daf1b74f7185
Tchoow <theo.lavie@live.fr>
2023-02-21 17:27:08 +0100

    feat: rename new solution

--- EasySave/remote_interface/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ EasySave/remote_interface/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -85,7 +85,7 @@ namespace remote_interface {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/remote_interface;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Remote_interface;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
